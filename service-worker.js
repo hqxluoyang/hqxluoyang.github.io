cc31ef34495f6ac112bfab6b3b3d3a7dacf708e7
houqinxin <houqingxin@xender.com>
2021-01-06 09:29:21 +0800

    lod

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53ef7826483a65db0f8f79277fdfc747.js"
+  "/precache-manifest.29dc9d836e6f2cc6d7817b4ba2b3fa72.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sanyuan_edite_topo"});
