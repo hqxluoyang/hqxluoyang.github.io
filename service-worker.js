eb6cd4a759e8531041176bc12c9ca0c3d6b9f7ed
houqingxin <hqxluoyang@163.com>
2021-01-08 23:04:43 +0800

    --

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29dc9d836e6f2cc6d7817b4ba2b3fa72.js"
+  "precache-manifest.4fc485cbc2556095fb40bea7f24ad307.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sanyuan_edite_topo"});
