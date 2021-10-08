89385a560aa266f2cf1fe21d716377983bbacae8
houqignxin <hqxluoyang@163.com>
2021-10-08 20:04:10 +0800

    -

---
+++ precache-manifest.9a71a35751bdd23c1bc782949308f5c3.js
@@ -0,0 +1,758 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d01f6f985b5cf2db11",
+    "url": "assets/css/app.css"
+  },
+  {
+    "revision": "72d01f6f985b5cf2db11",
+    "url": "assets/js/app.js"
+  },
+  {
+    "revision": "b0f1a414eb137f31296e",
+    "url": "assets/js/chunk-vendors.js"
+  },
+  {
+    "revision": "a083eff2220f7fb9b72725873d3a2bb3",
+    "url": "index.html"
+  },
+  {
+    "revision": "52e90621b1fc51e5116ba7eb0232b032",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "493db897cac9b6a5623e2116a1ab00a7",
+    "url": "static/alarm/LabelCard.png"
+  },
+  {
+    "revision": "7841218fc4aa948774bf0e657fe076d1",
+    "url": "static/alarm/LabelCardTight.png"
+  },
+  {
+    "revision": "2d23274d735fb660fd286866b3cc2483",
+    "url": "static/alarm/MonitorCard.png"
+  },
+  {
+    "revision": "d7c63052383910cb231d3c7f3b8e2145",
+    "url": "static/alarm/MonitorCardBlue.png"
+  },
+  {
+    "revision": "f2865928cf0d680a9d191f030f769728",
+    "url": "static/alarm/MonitorCardGreen.png"
+  },
+  {
+    "revision": "81f2959a8c6d6c2a63f691f89fbe38c3",
+    "url": "static/alarm/MonitorCardRed.png"
+  },
+  {
+    "revision": "be5ec88d4fa99010f31baae817e24fde",
+    "url": "static/alarm/MonitorCardTwo.png"
+  },
+  {
+    "revision": "096ad24d863898652688953c51e0f09e",
+    "url": "static/alarm/ProgressBar.png"
+  },
+  {
+    "revision": "b10e4fa842e73e2b0b3dc72711d7e8b2",
+    "url": "static/alarm/ProgressBarMask.png"
+  },
+  {
+    "revision": "abad55eb35ae6677b033cc7735fc52f8",
+    "url": "static/alarm/SimpleMonitorCard.png"
+  },
+  {
+    "revision": "d210ad978b31cbe8d240002c4addccf7",
+    "url": "static/alarm/alarm_early.png"
+  },
+  {
+    "revision": "c1c18ffbd08795156dfab231d3b4c714",
+    "url": "static/alarm/alarm_important.png"
+  },
+  {
+    "revision": "48d7507b7688a3e44f1d4ff79c47740e",
+    "url": "static/alarm/alarm_secondary.png"
+  },
+  {
+    "revision": "5479d21a37c87747a41146ae3c6627f5",
+    "url": "static/alarm/alarm_serious.png"
+  },
+  {
+    "revision": "69acbc1d722ff6ba75861f21a6869430",
+    "url": "static/alarm/alarm_urgent.png"
+  },
+  {
+    "revision": "02d99e37268f86341651aa81617d4187",
+    "url": "static/alarm/bubble+.png"
+  },
+  {
+    "revision": "a815550c121e9760596d4acce5e94dc1",
+    "url": "static/alarm/bubble-.png"
+  },
+  {
+    "revision": "d248e27e6437930f36ca0df4240de555",
+    "url": "static/alarm/camera_blue.png"
+  },
+  {
+    "revision": "fc72962adf5cdef50afbdd79758dbb2a",
+    "url": "static/alarm/camera_yellow.png"
+  },
+  {
+    "revision": "22083178f19c651a1fc87a04c0dcb0ed",
+    "url": "static/icon/CS路由器.png"
+  },
+  {
+    "revision": "9cfec9fac23e741fb726d20d78e3ac18",
+    "url": "static/icon/IPS设备.png"
+  },
+  {
+    "revision": "6277db7b3237991f89f58dfbc24260ac",
+    "url": "static/icon/SAN交换机.png"
+  },
+  {
+    "revision": "82fb8ba27ac7968036d5aa7ad09cf157",
+    "url": "static/icon/SAN汇聚交换机.png"
+  },
+  {
+    "revision": "944f6d4b301a84e87253ba0ad35bb7b4",
+    "url": "static/icon/UTM设备.png"
+  },
+  {
+    "revision": "36c8bac72ee50cd0386efce59ce80ea2",
+    "url": "static/icon/VM虚拟主机.png"
+  },
+  {
+    "revision": "9ed73b783aa38386e8d69e3abc03b6e6",
+    "url": "static/icon/VM路由控制器.png"
+  },
+  {
+    "revision": "5a00c2bd593ab4958484504127a473c8",
+    "url": "static/icon/VPN设备.png"
+  },
+  {
+    "revision": "7d22fd6ca92743b3c885c03dae25748f",
+    "url": "static/icon/WEB安全防护网关.png"
+  },
+  {
+    "revision": "ff450500ef5626ea1f200b4c7e7ff4bf",
+    "url": "static/icon/WEB服务器_.png"
+  },
+  {
+    "revision": "5702991b3e789c1d4a437af82020ed7e",
+    "url": "static/icon/bg1.png"
+  },
+  {
+    "revision": "7b06801ba24b8cb41a1aa83510ab69c0",
+    "url": "static/icon/bg10.png"
+  },
+  {
+    "revision": "db7cf1ff1bf534cfce5f8f449566c3c3",
+    "url": "static/icon/bg11.png"
+  },
+  {
+    "revision": "b8f32aacc9f9670d58d8ccea394d23fc",
+    "url": "static/icon/bg12.png"
+  },
+  {
+    "revision": "2669700f00b94683ba547f6ba7b325d5",
+    "url": "static/icon/bg13.png"
+  },
+  {
+    "revision": "ccd70e6f1e35544a2b77f1849cfc9615",
+    "url": "static/icon/bg2.png"
+  },
+  {
+    "revision": "c43dadf4e0dccbb1d38c8bda7efc2eba",
+    "url": "static/icon/bg3.png"
+  },
+  {
+    "revision": "fce34bca64ab318dc95ff96388b70b97",
+    "url": "static/icon/bg4.png"
+  },
+  {
+    "revision": "c41ecaec09383b9eee202f845c49e7df",
+    "url": "static/icon/bg5.png"
+  },
+  {
+    "revision": "92f5cc60aeaf1b91774db731aaba8c5c",
+    "url": "static/icon/bg6.png"
+  },
+  {
+    "revision": "306e70e5f87661cfdedf1bf4d0c1886d",
+    "url": "static/icon/bg7.png"
+  },
+  {
+    "revision": "8f7fe524041cb67c9639e82eb948727e",
+    "url": "static/icon/bg8.png"
+  },
+  {
+    "revision": "b4f4f1506844c8f38db13310862196f3",
+    "url": "static/icon/bg9.png"
+  },
+  {
+    "revision": "24465645816c2178efea1fa31974f126",
+    "url": "static/icon/blade_cage.png"
+  },
+  {
+    "revision": "7cadabeab68ed594ab0a6af0e40c10d0",
+    "url": "static/icon/distributing_cabinet.png"
+  },
+  {
+    "revision": "48982686d557b27706f02ed882ff5fb0",
+    "url": "static/icon/icnd.png"
+  },
+  {
+    "revision": "d75ddd3bf8da0684c0978b767f580f22",
+    "url": "static/icon/point.png"
+  },
+  {
+    "revision": "22083178f19c651a1fc87a04c0dcb0ed",
+    "url": "static/icon/router.png"
+  },
+  {
+    "revision": "15b6e4fd26dc4a4ef5122e49ea968bd8",
+    "url": "static/icon/router_.png"
+  },
+  {
+    "revision": "c3d83e1fd22fa99be2bb6bb2f65f8255",
+    "url": "static/icon/safety.png"
+  },
+  {
+    "revision": "628ac36b1e83b6192473b09daa9f8ec9",
+    "url": "static/icon/safety_.png"
+  },
+  {
+    "revision": "ff450500ef5626ea1f200b4c7e7ff4bf",
+    "url": "static/icon/server.png"
+  },
+  {
+    "revision": "019d00ec21c8b6e35da3049cd07e07a9",
+    "url": "static/icon/server_.png"
+  },
+  {
+    "revision": "25a125262492e826d9a5886e8594e180",
+    "url": "static/icon/storage.png"
+  },
+  {
+    "revision": "c35070563ca9a03021cf4bf36b5fc649",
+    "url": "static/icon/storage_.png"
+  },
+  {
+    "revision": "8f94f1b808b8577abf12963d763cf45b",
+    "url": "static/icon/switch.png"
+  },
+  {
+    "revision": "2158b9da049a02d5e1f4e3d01f230a6c",
+    "url": "static/icon/switch_.png"
+  },
+  {
+    "revision": "1bc4362cc33f38f1ac8d31f306afe642",
+    "url": "static/icon/video.png"
+  },
+  {
+    "revision": "110af7edc84f26071802134782ca502a",
+    "url": "static/icon/web服务器.png"
+  },
+  {
+    "revision": "291420c2325793d7ae05af5d584407f3",
+    "url": "static/icon/yun.png"
+  },
+  {
+    "revision": "f888b0947a1cf5602742efc2607c1c61",
+    "url": "static/icon/中端IPSAN存储.png"
+  },
+  {
+    "revision": "b4c0406638fbbb5236b25f40f27d173a",
+    "url": "static/icon/中端NAS存储.png"
+  },
+  {
+    "revision": "010e3877e79098fd415e7564f2158a57",
+    "url": "static/icon/中端SAN存储.png"
+  },
+  {
+    "revision": "2a860b51c7f67cce99d5db5d4bd4239d",
+    "url": "static/icon/井盖.png"
+  },
+  {
+    "revision": "9781fffe23dda6187f6d31b924f71357",
+    "url": "static/icon/交换机.ive"
+  },
+  {
+    "revision": "9cfec9fac23e741fb726d20d78e3ac18",
+    "url": "static/icon/入侵检测系统.png"
+  },
+  {
+    "revision": "ff6cf88c5f35dfd61896c95a8fe0f16f",
+    "url": "static/icon/入侵防御设备.png"
+  },
+  {
+    "revision": "15b6e4fd26dc4a4ef5122e49ea968bd8",
+    "url": "static/icon/其他.png"
+  },
+  {
+    "revision": "5cd77098365d854b2653fa57b1405408",
+    "url": "static/icon/刀片.png"
+  },
+  {
+    "revision": "b67f0f4d99b8f9fbf510d404cc5da96e",
+    "url": "static/icon/刀片服务器.png"
+  },
+  {
+    "revision": "24465645816c2178efea1fa31974f126",
+    "url": "static/icon/刀笼设备.png"
+  },
+  {
+    "revision": "efdcc730fa97e73409a7e9f2f74212b5",
+    "url": "static/icon/园区平面图.jpg"
+  },
+  {
+    "revision": "34a0ceb32d41dc77d877958d9e66a0f6",
+    "url": "static/icon/堡垒机.png"
+  },
+  {
+    "revision": "25a125262492e826d9a5886e8594e180",
+    "url": "static/icon/存储设备.png"
+  },
+  {
+    "revision": "e52bd0973d4f8e5364bd5f8ab3808485",
+    "url": "static/icon/安全审计系统.png"
+  },
+  {
+    "revision": "3bbcbd26cd4638e1a1c94f4a63120cd7",
+    "url": "static/icon/安全检测类.png"
+  },
+  {
+    "revision": "3bbcbd26cd4638e1a1c94f4a63120cd7",
+    "url": "static/icon/安全监测类.png"
+  },
+  {
+    "revision": "628ac36b1e83b6192473b09daa9f8ec9",
+    "url": "static/icon/安全设备.png"
+  },
+  {
+    "revision": "a04810d89c633fa15c93d3452c57d496",
+    "url": "static/icon/应用服务器.png"
+  },
+  {
+    "revision": "b5ff4f1339bde494fe75bb7a733b8a76",
+    "url": "static/icon/应用服务器_.png"
+  },
+  {
+    "revision": "89ce64e8435cd5d6cbb81873c12ad532",
+    "url": "static/icon/接入交换机.png"
+  },
+  {
+    "revision": "89ce64e8435cd5d6cbb81873c12ad532",
+    "url": "static/icon/接入交换机（小）.png"
+  },
+  {
+    "revision": "6efbae2e58302ee898d154e14aedfdab",
+    "url": "static/icon/接入设备.png"
+  },
+  {
+    "revision": "f205918704d3f53c59bf04bebfdf8344",
+    "url": "static/icon/接入设备_.png"
+  },
+  {
+    "revision": "765d5da9dee5de5b3e5dce0719a575c2",
+    "url": "static/icon/数据库服务器.png"
+  },
+  {
+    "revision": "261f190255207757d008af17f5dc4c21",
+    "url": "static/icon/数据库服务器_.png"
+  },
+  {
+    "revision": "134fd9024db081186b3a94d2db77e49f",
+    "url": "static/icon/文件服务器.png"
+  },
+  {
+    "revision": "15420fe2e2bd419825886a0e492235ad",
+    "url": "static/icon/文件服务器_.png"
+  },
+  {
+    "revision": "027743386cb42e195cc6f3c08f20efcf",
+    "url": "static/icon/服务器.png"
+  },
+  {
+    "revision": "7a4c19b13dc3066d8caa3743e1a1c0ca",
+    "url": "static/icon/服务器_.png"
+  },
+  {
+    "revision": "791f412fe437b6ed59108827a6f5651b",
+    "url": "static/icon/核心交换机.png"
+  },
+  {
+    "revision": "8f94f1b808b8577abf12963d763cf45b",
+    "url": "static/icon/核心交换机_.png"
+  },
+  {
+    "revision": "afb69f01b9205379861f1685c1259303",
+    "url": "static/icon/汇聚交换机.png"
+  },
+  {
+    "revision": "4093a71270964fef928969330df430e8",
+    "url": "static/icon/汇聚设备.png"
+  },
+  {
+    "revision": "a9c1f83f0593024fa47ed1b6d6a30088",
+    "url": "static/icon/汇聚设备_.png"
+  },
+  {
+    "revision": "1d42e3122fbaedc18067cf275d7117dd",
+    "url": "static/icon/流量监控系统.png"
+  },
+  {
+    "revision": "fb64195f3ae2082e96bf3d00662dad8b",
+    "url": "static/icon/漏洞扫描.png"
+  },
+  {
+    "revision": "48982686d557b27706f02ed882ff5fb0",
+    "url": "static/icon/网络设备.png"
+  },
+  {
+    "revision": "fe50ea7982f443c2592770d24a050303",
+    "url": "static/icon/网络隔离.png"
+  },
+  {
+    "revision": "6bd3e983725cd0b8f5957dfcb88f0c66",
+    "url": "static/icon/网闸.png"
+  },
+  {
+    "revision": "afb3dbef85810acb84d9e2e9fecfc60f",
+    "url": "static/icon/虚拟机.png"
+  },
+  {
+    "revision": "36c8bac72ee50cd0386efce59ce80ea2",
+    "url": "static/icon/虚拟机_.png"
+  },
+  {
+    "revision": "1dc2a15872da7fbecdaa1753d8e7fa3b",
+    "url": "static/icon/负载均衡设备.png"
+  },
+  {
+    "revision": "e8435c4118879d4a28b45923ebec612f",
+    "url": "static/icon/远程接入平台.png"
+  },
+  {
+    "revision": "548aebd521cda6675ff7600f55bb8b89",
+    "url": "static/icon/邮件服务器.png"
+  },
+  {
+    "revision": "1b29ba08ffe547a6c430c263d2e88317",
+    "url": "static/icon/邮件服务器_.png"
+  },
+  {
+    "revision": "ba63e4ef1f378f40d7989f251419d3b0",
+    "url": "static/icon/防DDOS系统.png"
+  },
+  {
+    "revision": "9393fa900433dd19dca6146568037f0b",
+    "url": "static/icon/防火墙.png"
+  },
+  {
+    "revision": "d5d341f869f79e727781e6bac12b9b5b",
+    "url": "static/icon/隔离装置.png"
+  },
+  {
+    "revision": "2e967e7322f2eb044bcb60d0c33fec40",
+    "url": "static/icon/高端SAN存储.png"
+  },
+  {
+    "revision": "55e7e18fd45431cc29f3c2dfdf4b46de",
+    "url": "static/imgs/activeLinks.png"
+  },
+  {
+    "revision": "08467c049bb6c81ce229f62b0e3a0831",
+    "url": "static/imgs/bj.png"
+  },
+  {
+    "revision": "a2597dfc0c503157666bb102686bddfa",
+    "url": "static/imgs/dj.png"
+  },
+  {
+    "revision": "66c8b15b23c08cd4ea71e05452997ae9",
+    "url": "static/imgs/history.png"
+  },
+  {
+    "revision": "83745b8ced88fee79bc8313ad7cec2dd",
+    "url": "static/imgs/icon/UPS.png"
+  },
+  {
+    "revision": "6b58933ea9623e8cd38d5714bceddac0",
+    "url": "static/imgs/icon/摄像头.png"
+  },
+  {
+    "revision": "8cd11122b0221727e9e6ed80ca6bf525",
+    "url": "static/imgs/icon/温湿度.png"
+  },
+  {
+    "revision": "97f530cfa0d8343d00613f15f2052b2c",
+    "url": "static/imgs/icon/空调.png"
+  },
+  {
+    "revision": "553e3c5acb9df928f3585ebd62f85dc3",
+    "url": "static/imgs/icon/配电柜.png"
+  },
+  {
+    "revision": "0029937f9223d8ecc6ed8dff32bb27c0",
+    "url": "static/imgs/icon/门.png"
+  },
+  {
+    "revision": "303edd9331a41034daffda38a3a4a457",
+    "url": "static/imgs/icons/door.svg"
+  },
+  {
+    "revision": "29177fea0031757887860d0ce1299014",
+    "url": "static/imgs/icons/left.png"
+  },
+  {
+    "revision": "237016d840fdf398380b5f73d54a1b0b",
+    "url": "static/imgs/icons/pillarCircle.svg"
+  },
+  {
+    "revision": "06cb06ff313c796198d7e84d53b9e9e9",
+    "url": "static/imgs/icons/pillarRect.svg"
+  },
+  {
+    "revision": "440e3e95384d78a2ba1303a2326f2e4d",
+    "url": "static/imgs/icons/right.png"
+  },
+  {
+    "revision": "6b8598004e9581e68581f2bf5a9b47bb",
+    "url": "static/imgs/icons/up.png"
+  },
+  {
+    "revision": "6b00566e6a7a54df0b83fe8a1d8b9427",
+    "url": "static/imgs/image.png"
+  },
+  {
+    "revision": "b4c3e02512126d949ec764cba75620af",
+    "url": "static/imgs/jc.png"
+  },
+  {
+    "revision": "13f4829310689a88e118efc7e4243ded",
+    "url": "static/imgs/jf.png"
+  },
+  {
+    "revision": "4be8bb34ea457ce8a7c5047429452df7",
+    "url": "static/imgs/main/1.png"
+  },
+  {
+    "revision": "4edfdb012502f10c5a3672c999dd03ae",
+    "url": "static/imgs/main/10.png"
+  },
+  {
+    "revision": "43216c93356459ba391daa4762ece427",
+    "url": "static/imgs/main/11.png"
+  },
+  {
+    "revision": "6ee1c1640efd298fd175bdd8aba995b1",
+    "url": "static/imgs/main/12.png"
+  },
+  {
+    "revision": "a92fad3436a601dbbc15eb38e0f577c1",
+    "url": "static/imgs/main/13.png"
+  },
+  {
+    "revision": "971c0da199fd650675eb875666e28235",
+    "url": "static/imgs/main/14.png"
+  },
+  {
+    "revision": "3f8ef72c9bece40c6bcacc0f8dd55d80",
+    "url": "static/imgs/main/15.png"
+  },
+  {
+    "revision": "b200f4929d83623b8ab790cd2ed8c945",
+    "url": "static/imgs/main/16.png"
+  },
+  {
+    "revision": "a21e9ac02655f4b1e1380b11f7ff0b39",
+    "url": "static/imgs/main/17.png"
+  },
+  {
+    "revision": "cd9790ee5d130159c018e1a33f05474d",
+    "url": "static/imgs/main/18.png"
+  },
+  {
+    "revision": "e0650324a2928b04ed6ad4b88bf1e628",
+    "url": "static/imgs/main/19.png"
+  },
+  {
+    "revision": "34910405a8b179a2082c670719be7faa",
+    "url": "static/imgs/main/2.png"
+  },
+  {
+    "revision": "9a9fb59745ac4f75cfe06274dcc70bc5",
+    "url": "static/imgs/main/20.png"
+  },
+  {
+    "revision": "48defcecbbe1a4f1a2a9e6c99a3bb11d",
+    "url": "static/imgs/main/21.png"
+  },
+  {
+    "revision": "85897e6f5a927047c7469732391812a7",
+    "url": "static/imgs/main/22.png"
+  },
+  {
+    "revision": "1eeae854de730df72077c927fd1a8c59",
+    "url": "static/imgs/main/23.png"
+  },
+  {
+    "revision": "291703f8581d878c82aa9292685dce8d",
+    "url": "static/imgs/main/24.png"
+  },
+  {
+    "revision": "a662bb39844ef5c6f818a0a6d2f9331e",
+    "url": "static/imgs/main/3.png"
+  },
+  {
+    "revision": "f6108c61e980ff28d8dc5f297fad2537",
+    "url": "static/imgs/main/4.png"
+  },
+  {
+    "revision": "3f6cd1e5e8c84115e55a8b3f3745d35f",
+    "url": "static/imgs/main/5.png"
+  },
+  {
+    "revision": "a5c2c8812f58d412b126d115df696dd6",
+    "url": "static/imgs/main/6.png"
+  },
+  {
+    "revision": "68b271c355d2d57ce83e1e086e1d22c0",
+    "url": "static/imgs/main/7.png"
+  },
+  {
+    "revision": "a7fe87d6dbfb2ba64cf058f8618160ca",
+    "url": "static/imgs/main/8.png"
+  },
+  {
+    "revision": "108cfc5593cb82a85dad9248eebbde9b",
+    "url": "static/imgs/main/9.png"
+  },
+  {
+    "revision": "9d352f4600c5e280befed55450c8f5e9",
+    "url": "static/imgs/main/bg.png"
+  },
+  {
+    "revision": "05b54b70cf5ef6627e4a9f3b556d6864",
+    "url": "static/imgs/main/bg1.png"
+  },
+  {
+    "revision": "36c1e6466d7b6ac6ad95c106b404fccd",
+    "url": "static/imgs/main/bg2.png"
+  },
+  {
+    "revision": "c72d5dcce5ee3d9e7fd6444c28a42f88",
+    "url": "static/imgs/main/bg3.png"
+  },
+  {
+    "revision": "d91bc64e4e1ea50e766e1f69d05c3e21",
+    "url": "static/imgs/main/botton.png"
+  },
+  {
+    "revision": "371d225c81b2a299fdd320aba8638074",
+    "url": "static/imgs/main/dfile.png"
+  },
+  {
+    "revision": "a9176abf59009f67b701d5229cdf938f",
+    "url": "static/imgs/main/importfile.png"
+  },
+  {
+    "revision": "4b89e4cfda125c64aeed88039c9d56c7",
+    "url": "static/imgs/main/logo.png"
+  },
+  {
+    "revision": "1a07a35a178082fc8a9f8a2c5c7604fd",
+    "url": "static/imgs/px.png"
+  },
+  {
+    "revision": "11998feb40ef3d5799b791333fdb158f",
+    "url": "static/imgs/reticle_icons.png"
+  },
+  {
+    "revision": "521269a374800e5580319914b5bcc06c",
+    "url": "static/imgs/rj.png"
+  },
+  {
+    "revision": "868be1bd11b1b22677fabfe25a8d109e",
+    "url": "static/imgs/rz.png"
+  },
+  {
+    "revision": "055b23a1c5b7fa2bfdffb36993dfe3bb",
+    "url": "static/imgs/sign.png"
+  },
+  {
+    "revision": "b80425bbf53402d499d54c86ca365870",
+    "url": "static/imgs/success.png"
+  },
+  {
+    "revision": "56e5e0d9b1d6360e58217463acdefb2c",
+    "url": "static/imgs/tj.png"
+  },
+  {
+    "revision": "38aac1c6091deab014d3835df563b3e6",
+    "url": "static/imgs/xnh.png"
+  },
+  {
+    "revision": "4122db91dfd6f030147f6d608f2d3441",
+    "url": "static/imgs/xt.png"
+  },
+  {
+    "revision": "2dfb9f6f565d56ca6f7ed7ac90399d11",
+    "url": "static/imgs/yh.png"
+  },
+  {
+    "revision": "c6cb3f01a32a5fca7bf5aa87a744e972",
+    "url": "static/imgs/yj.png"
+  },
+  {
+    "revision": "d535c2a9721ea6373b3b356b2810d3bd",
+    "url": "static/imgs/yw.png"
+  },
+  {
+    "revision": "49515928ee0eefd183846f1f6a91a44c",
+    "url": "static/model/PDU.FBX"
+  },
+  {
+    "revision": "40e06ccc0ff391f0668384332cd0819f",
+    "url": "static/model/UPS.FBX"
+  },
+  {
+    "revision": "c78bff6425c3504de0777c0856b05c29",
+    "url": "static/model/maps/海洛斯 空调1体黑色.jpg"
+  },
+  {
+    "revision": "f80bfedb4891983192c4af833d896da2",
+    "url": "static/model/摄像头.FBX"
+  },
+  {
+    "revision": "4c92922735e1afe9f4aaaf29e0ded7d3",
+    "url": "static/model/空调.FBX"
+  },
+  {
+    "revision": "dd37267d5c910df288b4c58f2c6734be",
+    "url": "static/tools/addGroup.png"
+  },
+  {
+    "revision": "1a0e9f5c4b39f1ede6b94e32178063a5",
+    "url": "static/tools/lineHS.png"
+  },
+  {
+    "revision": "cf8dc5e0bc747a764ba3802c36a88244",
+    "url": "static/tools/showLabel.png"
+  },
+  {
+    "revision": "9082eb144d2862b8732b50b2fb017549",
+    "url": "static/tools/微信图片_20200219172755.png"
+  },
+  {
+    "revision": "28e07a2963118d431c69dc3e02fab918",
+    "url": "static/tools/微信图片_20200219172807.png"
+  },
+  {
+    "revision": "28e07a2963118d431c69dc3e02fab918",
+    "url": "static/tools/微信图片_20200219172817.png"
+  }
+]);
